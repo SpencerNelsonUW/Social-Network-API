08843f14b7865380c41fd92f43dea971c7d9ca9a
SpencerNelsonUW <spencernelsonn@gmail.com>
2022-11-07 12:29:25 -0800

    setting up basic file structure and adding server and connection

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const db = require('./config/connection');
+
+//NEED TO IMPORT MODELS STILL
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//NEED TO IMPORT ROUTES OR CREATE HERE
+
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+    });
+  });
