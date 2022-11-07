f12536598da2850d399a5d29e826f4651cbe17b6
SpencerNelsonUW <spencernelsonn@gmail.com>
2022-11-07 12:59:03 -0800

    adding models

--- models/users.js
+++ models/users.js
@@ -1,1 +1,28 @@
 const mongoose = require('mongoose');
+
+const reactionSchema = new mongoose.Schema({
+    reactionBody:{type:String, required:true},
+    username:{type:String, required:true},
+})
+
+const thoughtSchema = new mongoose.Schema({
+    thoughtText: {type:String, required:true},
+    username:{type:String, required:true},
+    reactions:[reactionSchema],
+    reactionCount:{}
+})
+
+const friendSchema = new mongoose.Schema({
+    username:{type:String, required:true},
+    email:{type:String},
+})
+
+const userSchema = new mongoose.Schema({
+    username:{type:String, required:true},
+    email:{type:String, required:true},
+    thoughts:[thoughtSchema],
+    friends:[friendSchema],
+    friendCount:{}
+})
+
+module.exports = Users;
